a09b7dc144f754ee94a16a2534b7297af14bb9cf
kuro <kisuvitinh@gmail.com>
2020-09-07 21:00:09 +0700

    setup

---
+++ 01/operators.py
@@ -0,0 +1,8 @@
+a = 2
+b = 11.9
+
+print(b/a)
+print(b // a)
+
+for i in range(0, 4):
+    print(i)
---
+++ 01/string.py
@@ -0,0 +1,6 @@
+strNumber = '0123456789'
+
+print(strNumber[::-1])
+print(strNumber[7:5:-1])
+print(strNumber[8::-1])
+print(strNumber[-5::-1])
